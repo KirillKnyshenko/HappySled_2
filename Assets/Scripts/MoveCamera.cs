29c1a40d1e3fb66b87f0ea548ab6a7fa1cebef8b
KirillKnyshenko <83464065+KirillKnyshenko@users.noreply.github.com>
2021-11-16 20:13:56 +0300

    LocalRotate

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -4,34 +4,24 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    [SerializeField] private Vector3 offcetPostion, offcetRotation;
+    [SerializeField] private Vector3 offcetPostion;
     [SerializeField] private Transform target;
     [SerializeField] private float translateSpeed;
-    [SerializeField] private float rotationSpeed;
 
     private void Start()
     {
         target = GameManager.player.transform;
         HandleTranslation();
-        HandleRotation();
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         HandleTranslation();
-        HandleRotation();
     }
 
     private void HandleTranslation()
     {
-        var targetPosition = target.TransformPoint(offcetPostion);
+        var targetPosition = target.position + offcetPostion;
         transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
-    }
-
-    private void HandleRotation()
-    {
-        var direction = (target.position + offcetRotation) - transform.position;
-        var rotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 /// <summary>
 /// Стандартный скрипт игрока
 /// </summary>
+
 public class Player : MonoBehaviour
 {
-
     Rigidbody rb;
-    [SerializeField] float speedLR, posX;
+    [SerializeField] float speedLR, speedForward, posX, rotationY;
+    [SerializeField] Quaternion rotateLR, oldRotate;
 
     private void Start()
     {
@@ -19,13 +20,23 @@ public class Player : MonoBehaviour
 
         if (GameManager.gameStage == GameStage.Game)
         {
+            oldRotate = transform.rotation;
+            rotationY = Mathf.Lerp(rotationY, 0, Time.deltaTime);
+
+            // Move forward
+            transform.Translate(Vector3.forward * speedForward * Time.deltaTime);
+
+            // Left Right rotate
             if (Input.GetKey(KeyCode.Mouse0) && !Input.GetKeyDown(KeyCode.Mouse0))
             {
+                rotationY += Input.GetAxis("Mouse X");
+                rotationY = Mathf.Clamp(rotationY, -15, 15);
+                transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles, new Vector3(0, rotationY, 0), Time.deltaTime);
+                
                 posX += Input.GetAxis("Mouse X") * speedLR * Time.deltaTime;
-                posX = Mathf.Clamp(posX, -14.5f, 14.5f);
+                posX = Mathf.Clamp(posX, -5.5f, 5.5f);
                 transform.position = Vector3.Lerp(transform.position, new Vector3(posX, transform.position.y, transform.position.z), speedLR * Time.deltaTime);
             }
-            
         }
     }
 }
